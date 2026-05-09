547146d055735650a2e9599ba065b6a93f0d8c18
tyrellcisket-ctrl <tyrellcisket@gmail.com>
2026-05-09 11:00:57 +0800

    Update gcc-wrapper.py

--- scripts/gcc-wrapper.py
+++ scripts/gcc-wrapper.py
@@ -84,7 +84,7 @@ def run_gcc():
     try:
         proc = subprocess.Popen(args, stderr=subprocess.PIPE)
         for line in proc.stderr:
-            print line,
+            print(line, end="")
             interpret_warning(line)
 
         result = proc.wait()
